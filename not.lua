8121e3a80ff8edf80340bc7760c1b5e65b54cf7d
HayateXhublua <tp777927@gmail.com>
2026-06-16 19:13:21 +0700

    Create not.lua

---
+++ not.lua
@@ -0,0 +1,94 @@
+local placeId = game.PlaceId
+
+local ZombieArenaPlaces = {
+    [73403837686778] = true,
+    [76889882866148] = true,
+    [98653711640137] = true,
+    [98927955463992] = true,
+    [114204398207377] = true
+}
+
+local BloxFruitsPlaces = {
+    [2753915549] = true,
+    [4442272183] = true,
+    [7449423635] = true,
+    [73902483975735] = true,
+    [76401440271920] = true,
+    [79091703265657] = true,
+    [85211729168715] = true,
+    [92968389658553] = true,
+    [95165932064349] = true,
+    [100117331123089] = true,
+    [101151419317285] = true,
+    [113741252407134] = true,
+    [114279672983750] = true,
+    [117896981438898] = true,
+    [122478697296975] = true
+}
+
+local KingLegacyPlaces = {
+    [4520749081] = true,
+    [5931540094] = true,
+    [6381829480] = true,
+    [15759515082] = true
+}
+
+local BuildABoatPlaces = {
+    [537413528] = true,
+    [1930665568] = true,
+    [1930863474] = true,
+    [1930866268] = true
+}
+
+local PickaxeTycoonPlaces = {
+    [73814003954154] = true
+}
+
+local MurderMystery2Places = {
+    [142823291] = true
+}
+
+local function Notify(text)
+    pcall(function()
+        game:GetService("StarterGui"):SetCore("SendNotification", {
+            Title = "SEI Hub",
+            Text = text,
+            Icon = "rbxassetid://99118312841228",
+            Duration = 5
+        })
+    end)
+end
+
+if placeId == 8657766101 then
+    Notify("Success!")
+    loadstring(game:HttpGet("https://pastefy.app/GruPjoEA/raw"))()
+
+elseif ZombieArenaPlaces[placeId] then
+    Notify("Success!")
+    loadstring(game:HttpGet("https://pastefy.app/tOkBNckx/raw"))()
+
+elseif BloxFruitsPlaces[placeId] then
+    Notify("Success!")
+    loadstring(game:HttpGet("https://xhider.xyz/raw/Xhider/Bot2/kszm08kwvx.lua"))()
+
+elseif KingLegacyPlaces[placeId] then
+    Notify("Success!")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/tp777927-byte/Loader/main/obfuscated.lua%20(12).txt"))()
+
+elseif BuildABoatPlaces[placeId] then
+    Notify("Success!")
+    loadstring(game:HttpGet("https://pastefy.app/liSKujPu/raw"))()
+
+elseif PickaxeTycoonPlaces[placeId] then
+    Notify("Success!")
+    loadstring(game:HttpGet("https://pastefy.app/QBsEm8gc/raw"))()
+
+elseif MurderMystery2Places[placeId] then
+    Notify("Success!")
+    loadstring(game:HttpGet("https://xhider.xyz/raw/Xhider/Bot2/go35dqe86o.txt"))()
+
+else
+    Notify("แมพนี้ไม่รองรับ")
+    task.wait(2)
+    game.Players.LocalPlayer:Kick("❌ แมพนี้ไม่รองรับโดย SEI Hub")
+end
